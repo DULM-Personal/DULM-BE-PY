4920405c5039b15bd64e2a71b8db84b177657662
imhanseong <hans.hanseong@gmail.com>
2026-01-13 14:31:40 +0900

    fix :: url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from .views import (
 urlpatterns = [
     path("send-code", SendEmailCodeView.as_view()),
     path("verify-code", VerifyEmailCodeView.as_view()),
-    path("register", RegisterView.as_view()),
+    path("signup", RegisterView.as_view()),
     path("login", LoginView.as_view()),
     path("me", MeView.as_view()),
     path("reset-password", ResetPasswordView.as_view()),
